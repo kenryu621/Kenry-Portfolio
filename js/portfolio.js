3eb225bdf9631d215ae5489168208cf85a886435
Yifan Yu <kenryu422@outlook.com>
2019-04-05 22:15:23 -0700

    Feat: Contact page finished

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,15 @@
 function changePosition() {
-  let offPosition = window.pageYOffset;
-  if (document.getElementsByClassName('websiteShot')[0].id == 'interestShot') {
-    offPosition = offPosition + 200;
-  };
+  var offPosition = window.pageYOffset;
+  //if (document.getElementsByClassName('websiteShot')[0].id == 'interestShot') {
+  //  offPosition = offPosition + 200;
+  //}
+  switch (document.getElementsByClassName('websiteShot')[0].id) {
+    case 'interestShot':
+      offPosition = offPosition + 200;
+      break;
+    case 'contactShot':
+      offPosition = offPosition + 150;
+      break;
+  }
   document.getElementsByClassName('websiteShot')[0].setAttribute('style', 'background-position-y: -' + offPosition + 'px');
 }
